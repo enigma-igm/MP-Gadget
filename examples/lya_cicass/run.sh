4d07f5981fa44c13b471318c10b0ebfc8261379a
Frederick Davies <davies@physics.ucsb.edu>
2021-12-11 18:16:50 +0100

    updated cicass example

--- examples/lya_cicass/run.sh
+++ examples/lya_cicass/run.sh
@@ -1,2 +1,15 @@
-mpirun -np 1 ../../genic/MP-GenIC-CICASS paramfile.genic || exit 1
-mpirun -np 1 ../../gadget/MP-Gadget paramfile.gadget || exit 1
+# First, convert the CICASS ICs to MP-Gadget format
+# This needs to be done with one process and one thread.
+# (note that cdm and xray use the same ICs)
+export OMP_NUM_THREADS=1
+mpirun -np 1 ../../genic/MP-GenIC-CICASS paramfile_cdm.genic || exit 1
+mpirun -np 1 ../../genic/MP-GenIC-CICASS paramfile_wdm.genic || exit 1
+
+# Now run the hydro
+# Threads optimized for igm, but you might need NMPI=1 on Mac
+export NMPI=32
+export OMP_NUM_THREADS=4
+# Each of these takes about 5 minutes to run to z = 5 on igm.
+mpirun -np $NMPI --use-hwthread-cpus ../../gadget/MP-Gadget paramfile_cdm.gadget || exit 1
+mpirun -np $NMPI --use-hwthread-cpus ../../gadget/MP-Gadget paramfile_wdm.gadget || exit 1
+mpirun -np $NMPI --use-hwthread-cpus ../../gadget/MP-Gadget paramfile_xray.gadget || exit 1
